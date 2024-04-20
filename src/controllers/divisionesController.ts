e5b08d47fde7faa3ea42702acd77262201ea7958
Erick Guerrero <erickg@valles.udg.mx>
2024-04-20 10:11:17 -0600

    explicación CRUD

--- src/controllers/divisionesController.ts
+++ src/controllers/divisionesController.ts
@@ -1,6 +1,13 @@
 import { RequestHandler } from "express";
 import { Division } from "../models/division";
 
+/**
+ * Create -> crear un recurso (insert)
+ * Read -> leer recursos (select)
+ * Update -> actualiza un registro en la db (update)
+ * Delete -> eliminar un registro (o varios) en la db (delete)
+ */
+
 export const test:RequestHandler = async (req,res)=>{
     return res.status(200).send("Hola desde división controller");
 }
